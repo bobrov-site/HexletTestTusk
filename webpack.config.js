fd46a4a388b24bee63dee49de55a845544e79dbf
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-03 20:38:30 +0300

    configurate webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        assetModuleFilename: '[name][ext]'
     },
     devServer: {
         static: './dist',
@@ -53,12 +54,15 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 
-                'asset/resource',
+                type: 'asset/resource',
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
             },
         ]
     },
